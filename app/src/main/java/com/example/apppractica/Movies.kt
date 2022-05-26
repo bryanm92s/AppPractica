8c7563c66440dc47b64aca0e5ec3527460f7802d
bryanm92s <bryanmorales8240@gmail.com>
2022-05-26 15:40:19 -0500

    Se implemente Binding al pryecto y se modifica Binding en MainActivity

---
+++ app/src/main/java/com/example/apppractica/Movies.kt
@@ -0,0 +1,5 @@
+package com.example.apppractica
+
+data class Movies(var name:String, var Country: String, var logo:Int){
+
+}
--- app/src/main/java/com/example/apppractica/Views/MainActivity.kt
+++ app/src/main/java/com/example/apppractica/Views/MainActivity.kt
@@ -3,24 +3,37 @@ package com.example.apppractica.Views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.apppractica.R
+import com.example.apppractica.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController:NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Instancia del view binding
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        //navController = navHostFragment.navController
+        //val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        //setupWithNavController(bottomNavigationView,navController)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
-        navController = navHostFragment.navController
-        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        setupWithNavController(bottomNavigationView,navController)
+        val navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNavigationView
+        // Sincronizacion del navController con nuesto bottom navigation
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
